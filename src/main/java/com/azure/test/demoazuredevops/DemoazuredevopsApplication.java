e9664ef2664f5ec3ed9ac8b32cfaecb65036cdfa
subhranshu pujari <subh.pujari@gmail.com>
2018-12-19 22:40:17 +0530

    test

---
+++ src/main/java/com/azure/test/demoazuredevops/DemoazuredevopsApplication.java
@@ -0,0 +1,14 @@
+package com.azure.test.demoazuredevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoazuredevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoazuredevopsApplication.class, args);
+	}
+
+}
+
